12408898976f6fec8cb6bca835cfd2303a331ffd
Andrey Vinícius Jodar <andreyviniciusjodar@gmail.com>
2025-08-07 13:32:30 -0300

    first commit

---
+++ backend/src/test/java/com/github/andreyjodar/todo_challenge/TodoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andreyjodar.todo_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
